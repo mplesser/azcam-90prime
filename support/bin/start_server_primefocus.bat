567a2db6054f2cbaae27bdc3210e25e2bb63d442
Michael Lesser <mlesser@arizona.edu>
2022-06-07 10:02:25 -0700

    6K update

--- support/bin/start_server_primefocus.bat
+++ support/bin/start_server_primefocus.bat
@@ -1,3 +1,3 @@
 @echo off
 
-ipython.exe --profile azcamserver --TerminalInteractiveShell.term_title_format=azcamserver -i -m azcam_90prime.server -- -system normal
+ipython.exe --profile azcamserver --TerminalInteractiveShell.term_title_format=azcamserver -i -m azcam_90prime.server -- -lab -system 6k
