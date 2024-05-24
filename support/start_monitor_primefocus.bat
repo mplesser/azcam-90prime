d45a19ad2b3bd43e3db4fb4287708079f72a41f9
Michael Lesser <mlesser@arizona.edu>
2024-05-24 10:38:00 -0700

    initial update for dev version

--- support/start_monitor_primefocus.bat
+++ support/start_monitor_primefocus.bat
@@ -1,3 +1,3 @@
 @echo off
 
-start/min "azcammonitor" python -m azcam_server.monitor.azcammonitor -configfile "/data/90prime/parameters/parameters_monitor_90prime.ini"
+azcammonitor -configfile /data/90prime/parameters/parameters_monitor_90prime.ini
